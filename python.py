1e9ef2b4189952fdfe2225e2d169c72ea92401e1
kadir-zeren <k4dzeren@gmail.com>
2024-10-16 12:00:40 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,11 @@
+school_records={
+"personal_info":
+{"kid":{"tom": {"class":"intermediate", "age":10},
+"sue": {"class": "elementary", "age":8}
+},
+"teen":{"joseph": {"class": "college", "age":19},
+"marry":{"class": "high school", "age": 16}
+},
+},
+}
+print(school_records['personal_info']['teen'] ['marry'] ['age'])
